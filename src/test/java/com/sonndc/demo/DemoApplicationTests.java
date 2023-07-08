6804fa58911b282d0fac1b6140e32dfa433e0326
Công Sơn <sonnguyen1129@gmail.com>
2023-07-08 11:07:57 +0700

    first init

---
+++ src/test/java/com/sonndc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonndc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
